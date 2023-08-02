c022feca3f1341854a7a0759c351b2b5d913a432
Eleni Shor <eleni.shor@proton.me>
2023-08-02 17:10:14 -0400

    rewrite test of 'make_grad_fn'

--- test_influence.py
+++ test_influence.py
@@ -26,12 +26,14 @@ def test_make_loss_fn():
     weights = torch.tensor([[1.0, 2.0, 3.0]])  # (1, 3)
     input = torch.tensor([1.0, 5.0, 3.0])  # (3,)
     target = torch.tensor(18.0)  # ()
-    # expected_output = weights @ input = 1 * 1 + 2 * 5 + 3 * 3 = 20
-    # expected_loss = 0.5 * (expected_output - target) ** 2 = 0.5 * (20 - 18) ** 2 = 2
-    expected_loss = torch.tensor(2.0)
 
     model = make_linear_model_from_weights(weights)
     params = {name: param.detach() for name, param in model.named_parameters()}
+
+    # expected:
+    # output = weights @ input = 1 * 1 + 2 * 5 + 3 * 3 = 20
+    # loss = 0.5 * (output - target) ** 2 = 0.5 * (20 - 18) ** 2 = 2
+    expected_loss = torch.tensor(2.0)
 
     loss_fn = make_loss_fn(model)
     loss = loss_fn(params, input, target)
@@ -39,26 +41,25 @@ def test_make_loss_fn():
     assert torch.allclose(loss, expected_loss)
 
 
-@pytest.mark.parametrize("in_features", [1, 5, 10])
-def test_make_grad_fn(in_features):
-    """
-    Test 'make_grad_fn' on a linear model.
-    """
-    model = nn.Sequential(
-        nn.Linear(in_features=in_features, out_features=1, bias=False),
-        Rearrange("1 ->"),
-    )
-    model.zero_grad()
+def test_make_grad_fn():
+    """Test 'make_grad_fn' on a linear model."""
+    weights = torch.tensor([[1.0, 2.0, 3.0]])  # (1, 3)
+    input = torch.tensor([1.0, 5.0, 3.0])  # (3,)
+    target = torch.tensor(18.0)  # ()
+
+    model = make_linear_model_from_weights(weights)
     params = {name: param.detach() for name, param in model.named_parameters()}
+
+    # expected:
+    # output = weights @ input = 1 * 1 + 2 * 5 + 3 * 3 = 20
+    # loss = 0.5 * (output - target) ** 2 = 0.5 * (20 - 18) ** 2 = 2
+    # grad = dloss/dweights = dloss/doutput * doutput/dweights
+    # dloss/doutput = output - target = 20 - 18 = 2
+    # doutput/dweights = input = [1, 5, 3]
+    # grad = dloss/doutput * doutput/dweights = (output - target) * input = 2 * [1, 5, 3] = [2, 10, 6]
+    expected_grad = torch.tensor([2.0, 10.0, 6.0])
+
     grad_fn = make_grad_fn(model)
-
-    input = torch.randn(size=(in_features,))
-    output = model(input)
-    target = torch.tensor(0.0)
-
-    # loss = 0.5 * output ** 2
-    # dloss/doutput = output
-    # dloss/dparams = dloss/doutput * doutput/dparams = output * input
-    grads = grad_fn(params, input, target)
-    assert grads.shape == (in_features,)
-    assert torch.allclose(grads, output * input)
+    grad = grad_fn(params, input, target)
+    assert grad.shape == (3,)
+    assert torch.allclose(grad, expected_grad)
