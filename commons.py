7c24e9e48d4fa217d9d07172b9205c004c57abf3
Eleni Shor <leni@mit.edu>
2023-08-17 14:27:59 -0400

    add checkpoint saving

--- commons.py
+++ commons.py
@@ -11,6 +11,8 @@ Params = dict[str, Array]
 
 
 DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
+DATA_DIR: str = "/om2/user/leni/influence/data"
+MODEL_DIR: str = "/om2/user/leni/influence/models"
 
 
 class Batch:
--- train.py
+++ train.py
@@ -1,13 +1,20 @@
+import torch
 import torch.nn.functional as F
 
+from pathlib import Path
+
+from einops import rearrange
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+from tqdm import trange
 
-from commons import IndexedDataset
+from commons import DEVICE, DATA_DIR, MODEL_DIR, IndexedDataset
 from models import make_mlp
 
+
+RUN_NAME: str = "MNIST-10-epochs"
 
 # MNIST dataset
 IN_FEATURES: int = 28 * 28
@@ -19,17 +26,31 @@ NUM_HIDDEN_LAYERS: int = 4
 
 # training hyperparameters
 BATCH_SIZE: int = 32
-NUM_EPOCHS: int = 1
+NUM_EPOCHS: int = 10
 LEARNING_RATE: float = 1e-3
 
 
 if __name__ == "__main__":
+    # make run directory
+    run_dir = Path(f"{MODEL_DIR}/{RUN_NAME}")
+    run_dir.mkdir(exist_ok=True)
+
     # make datasets and dataloaders
     train_dataset = IndexedDataset(
-        MNIST(root="data/", train=True, transform=ToTensor(), download=True)
+        MNIST(
+            root=f"{DATA_DIR}",
+            train=True,
+            transform=ToTensor(),
+            download=True,
+        )
     )
     test_dataset = IndexedDataset(
-        MNIST(root="data/", train=False, transform=ToTensor(), download=True)
+        MNIST(
+            root=f"{DATA_DIR}",
+            train=False,
+            transform=ToTensor(),
+            download=True,
+        )
     )
     train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
@@ -40,16 +61,30 @@ if __name__ == "__main__":
         out_features=OUT_FEATURES,
         hidden_features=HIDDEN_FEATURES,
         num_hidden_layers=NUM_HIDDEN_LAYERS,
+        device=DEVICE,
     )
 
     # make optimizer
     optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
 
     # training loop
-    for epoch in range(NUM_EPOCHS):
+    for epoch in trange(1, NUM_EPOCHS + 1):
         for indices, inputs, targets in train_dataloader:
+            # move data to device
+            indices = indices.to(device=DEVICE)
+            inputs = inputs.to(device=DEVICE)
+            targets = targets.to(device=DEVICE)
+
+            # flatten input images
+            inputs = rearrange(inputs, "b c h w -> b (c h w)")
+
+            # take one optimization step
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = F.cross_entropy(outputs, targets)
             loss.backward()
             optimizer.step()
+
+        # save model
+        state_dict = model.state_dict()
+        torch.save(state_dict, f"{MODEL_DIR}/{RUN_NAME}/{epoch}.pt")
